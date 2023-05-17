f3e53532657d5282479fb33846ef495f80097025
DJ Oakman <djoakman512@gmail.com>
2023-05-17 04:06:19 +0000

    Sync LeetCode submission Runtime - 34 ms (5.14%), Memory - 43.3 MB (99.95%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,14 +1,24 @@
 import java.util.HashMap;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        HashMap<Character, Integer> sMap = new HashMap<Character, Integer>();
-        HashMap<Character, Integer> tMap = new HashMap<Character, Integer>();
-        for (int a = 0; a < s.length(); a++) {
-            sMap.merge(s.charAt(a), 1, Integer::sum);
-            tMap.merge(t.charAt(a), 1, Integer::sum);
-        }
-        return (sMap.equals(tMap));
-        
+        if (s.length() != t.length()) 
+         return false;
+     
+      HashMap<Character, Integer> map = new HashMap<>();
+      HashMap<Character, Integer> map2 = new HashMap<>();
+      
+      for (int i = 0; i < s.length(); i++) {
+         if (!map.containsKey(s.charAt(i))) map.put(s.charAt(i), 1);
+         else {
+            map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+         }
+         if (!map2.containsKey(t.charAt(i))) map2.put(t.charAt(i), 1);
+         else {
+            map2.put(t.charAt(i), map2.get(t.charAt(i)) + 1);
+         }
+         
+      }
+      return map.equals(map2);  
     }
 }
