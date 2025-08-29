763b1558a76a321004c33d7883676384ac841dea
DJ Oakman <djoakman512@gmail.com>
2025-08-29 02:11:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

---
+++ 0812-rotate-string/solution.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+    
+        
+        if len(s) != len(goal):
+            return False
+        
+        if s in (goal + goal):
+            return True
+        return False
+        
